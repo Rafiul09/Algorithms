830b48d73d2997b804121891a1ee5e84186613b4
Rafiul <104949652+Rafiul09@users.noreply.github.com>
2022-10-24 09:56:12 +0600

    Create Rangen2.cpp

---
+++ Rangen2.cpp
@@ -0,0 +1,16 @@
+
+#include<bits/stdc++.h>
+#include <iostream>
+ using namespace std;
+
+ int main()
+ {
+     srand(time(0));
+     freopen("in.txt","w",stdout);
+     //int a[100000];
+     for(int i=0;i<1000000;i++)
+     {
+        cout<< rand()<<endl;
+         //cout<<a[i]<<"\n";
+     }
+ }
