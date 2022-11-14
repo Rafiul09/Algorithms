dc503694fb81e5c1ae0d588934d1ae27f5e6c471
Rafiul <104949652+Rafiul09@users.noreply.github.com>
2022-11-14 09:30:19 +0600

    Add files via upload

---
+++ final/graph2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+ using namespace std;
+
+ void printGraph(vector<int>rslt[],int node)
+ {
+     for(int i = 0; i < node; i++){
+            cout <<i<<"-->";
+        for(int j = 0; j < rslt[i].size(); j++){
+            cout << rslt[i][j] << " ";
+        }
+        cout << "\n";
+    }
+ }
+ int main()
+ {
+     int node, edge,u,v;
+     cout<<"Enter node Then edge\n";
+     cin>>node>>edge;
+     vector<int>arr[node];
+
+
+
+     for(int i=0; i<edge; i++)
+     {
+         cin>>u>>v;
+          arr[u].push_back(v);
+          arr[v].push_back(u);
+     }
+
+     printGraph(arr,node);
+ }
+
